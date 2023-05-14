786c82973c0cd7337313263cf065b27111acc184
devstackpro <89517315+devstackpro@users.noreply.github.com>
2023-05-14 00:40:20 -0300

    configurando contato e blog

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -153,10 +153,10 @@ const RenderPost = ({ post, redirect, preview }) => {
       <div className={blogStyles.post}>
         <h1>{post.Page || ''}</h1>
         {post.Authors.length > 0 && (
-          <div className="authors">By: {post.Authors.join(' ')}</div>
+          <div className="authors">Por: {post.Authors.join(' ')}</div>
         )}
         {post.Date && (
-          <div className="posted">Posted: {getDateStr(post.Date)}</div>
+          <div className="posted">Postado em: {getDateStr(post.Date)}</div>
         )}
 
         <hr />
